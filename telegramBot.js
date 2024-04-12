5aef1643b9ed543e0abb9984a304fdb21dd2d02c
bigracks223 <166836623+bigracks223@users.noreply.github.com>
2024-04-12 19:21:19 +0100

    Update telegramBot.js

--- telegramBot.js
+++ telegramBot.js
@@ -1,11 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api');
-
 const botToken = "6982147281:AAF8tTMmMuLuTohDSIg1U9RpvzLZ0Eidv20";
 const bot = new TelegramBot(botToken, { polling: true });
 const chatId = "Y7185443526";
 
 function sendNotification(message) {
-  bot.sendMessage(chatId, message);
+  const options = {
+    chat_id: chatId,
+    text: message,
+  };
+
+  bot.sendMessage(options, function (error, response) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("Message sent: " + response.message_id);
+    }
+  });
 }
 
 module.exports = { sendNotification };
